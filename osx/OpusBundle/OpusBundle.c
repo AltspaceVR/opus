3cb8f5891a70331b507f6933a5e2197411d05261
Marshall Quander <marshall@quander.me>
2016-07-29 15:44:11 -0700

    Add OS X build toolchain

---
+++ osx/OpusBundle/OpusBundle.c
@@ -0,0 +1,11 @@
+//
+//  OpusBundle.c
+//  OpusBundle
+//
+//  Created by Sohail Shafii on 7/6/15.
+//  Copyright (c) 2015 Sohail Shafii. All rights reserved.
+//
+
+#include "OpusBundle.h"
+
+// C file necessary for linking to work
---
+++ osx/OpusBundle/OpusBundle.h
@@ -0,0 +1,14 @@
+//
+//  OpusBundle.h
+//  OpusBundle
+//
+//  Created by Sohail Shafii on 7/6/15.
+//  Copyright (c) 2015 Sohail Shafii. All rights reserved.
+//
+
+#ifndef __OpusBundle__OpusBundle__
+#define __OpusBundle__OpusBundle__
+
+#include <stdio.h>
+
+#endif /* defined(__OpusBundle__OpusBundle__) */
